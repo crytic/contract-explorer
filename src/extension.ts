1bd42248a39547685b4fabf87a595aa7cca530a6
Omidiora Samuel <8148384+samparsky@users.noreply.github.com>
2019-02-17 17:53:32 +0100

    fix: refactor code

--- src/extension.ts
+++ src/extension.ts
@@ -25,71 +25,98 @@ export function activate(context: vscode.ExtensionContext) {
         await checkSlitherVersion();
 
         const { include, exclude } = getConfiguration('slither');
+        const result = isValidDetectors({ include, exclude }, outputChannel);
+
+        if(!result) {
+            console.log("processing")
+            outputChannel.show();
+            return;
+        }
+        
         const outputDir  = `${workspacePath}/.slither`;
         const outputFile = `${outputDir}/output.json`;
 
         shell.mkdir("-p", outputDir);
 
-        let cmd = `slither ${workspacePath} --disable-solc-warnings --json ${outputFile}`;
-        
-        if(include.length > 0){
-            const result = await checkDetectors(include, outputChannel);
-            if(!result) {
-                return;
-            }
-            cmd = `${cmd} --detect ${include.join(',')}`;
-        }
+        let cmd : string = `slither ${workspacePath} --disable-solc-warnings --json ${outputFile}`;
+        cmd = await addFlag(include, cmd, `detect`);
+        cmd = await addFlag(exclude, cmd, `exclude`);
 
-        if(exclude.length > 0){
-            const result = await checkDetectors(exclude, outputChannel);
-            if(!result) {
-                return;
-            }
-            cmd = `${cmd} --exclude ${exclude.join(',')}`;
-        }
-
-
-        let err = null;
+        let err : Error | null = null;
         await exec(cmd).catch((e: Error) => err = e);
 
         if(err) {
-            let data = JSON.parse(fs.readFileSync(outputFile, 'utf8'));
-            data = sortError(data);
-
-            data.forEach((item: any) => {
-                const descriptions = item['description'].replace(/#/g, ":").replace(/\t/g, "").split("\n");
-
-                descriptions.forEach( (description: any) => {
-                    if(description === "") {
-                        return;
-                    }
-                    if(!description.startsWith("-")){
-                        outputChannel.appendLine("")
-                    }
-                    if(description.startsWith("-")){
-                        outputChannel.appendLine(`\t${description}`);
-                    } else {
-                        outputChannel.appendLine(`${description}`);
-                    }
-                });
-            });            
+            if(fs.existsSync(outputFile)){
+                let data = JSON.parse(fs.readFileSync(outputFile, 'utf8'));
+                data = sortError(data);
+                parseResponse(data, outputChannel);
+            } else {
+                outputChannel.appendLine(err!.toString());
+            }
+        } else {
+            outputChannel.appendLine("No issues detected :D");
         }
 
         outputChannel.show();
 
         shell.rm(`${outputDir}/*`);
-        // Display a message box to the user
     });
 
     context.subscriptions.push(slither);
 }
 
+async function addFlag(option: [], cmd: string, flag: string) : Promise<string> {
+    if(option.length > 0){
+        cmd = `${cmd} --${flag} ${option.join(',')}`;
+    }
+    return cmd;
+}
+
+async function parseResponse(data: [], outputChannel: vscode.OutputChannel){
+    data.forEach((item: any) => {
+        const descriptions = item['description'].replace(/#/g, ":").replace(/\t/g, "").split("\n");
+
+        descriptions.forEach( (description: any) => {
+            if(description === "") {
+                return;
+            }
+            if(!description.startsWith("-")){
+                outputChannel.appendLine("");
+            }
+            if(description.startsWith("-")){
+                outputChannel.appendLine(`\t${description}`);
+            } else {
+                outputChannel.appendLine(`${description}`);
+            }
+        });
+    });            
+}
+
+async function isValidDetectors(options: {'exclude':[], 'include': []}, outputChannel: vscode.OutputChannel){
+    let isValid = false;
+
+    if(options.include.length > 0){
+        isValid = await checkDetectors(options.include, outputChannel);
+        if(!isValid) {
+            return isValid;
+        }
+    }
+
+    if(options.exclude.length > 0){
+        isValid = await checkDetectors(options.exclude, outputChannel);
+        if(!isValid) {
+            return isValid;
+        }
+    }
+
+    return isValid;
+}
+
 async function checkDetectors(detectors: any, outputChannel: vscode.OutputChannel){
-    detectors= detectors.filter( (item: string)=> item !== "" );
+    detectors = detectors.filter( (item: string)=> item !== "" );
     const isValid = await validateDetectors(detectors);
     if(!isValid){
         outputChannel.appendLine(`Error: Invalid detectors present Detectors: ${detectors}`);
-        outputChannel.show();
         return false;
     }
     return true;
